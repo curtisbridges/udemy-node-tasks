425631b4df0a88ae6aef31a826a504c14f4490dc
Curtis Bridges <curtis@curtisbridges.com>
2020-10-10 15:22:06 -0400

    use mongoose models for tasks and users
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 const express = require('express')
+require('./db/mongoose') // ensures mongoose connects, nothing else
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
 // REST API here
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((error) => {
+        console.log("Error creating user", error)
+        res.status(400).send(error)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
