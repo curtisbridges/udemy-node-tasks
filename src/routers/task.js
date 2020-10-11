16344c9ff673056f44ffb46f933dc5d424853f27
Curtis Bridges <curtis@curtisbridges.com>
2020-10-11 11:07:44 -0400

    hash user passwords and use save middleware
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- src/routers/task.js
+++ src/routers/task.js
@@ -4,70 +4,78 @@ const task = require('../models/task')
 const router = new express.Router()
 
 router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+  const task = new Task(req.body)
 
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (error) {
-        res.status(400).send(error)
-    }
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (error) {
+    res.status(400).send(error)
+  }
 })
 
 router.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (error) {
-        res.status(500).send()
-    }
+  try {
+    const tasks = await Task.find({})
+    res.send(tasks)
+  } catch (error) {
+    res.status(500).send()
+  }
 })
 
 router.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
+  const _id = req.params.id
 
-    try {
-        const task = Task.findById(_id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (error) {
-        res.status(500).send(error)
+  try {
+    const task = Task.findById(_id)
+    if (!task) {
+      return res.status(404).send()
     }
+    res.send(task)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
 router.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    // remove atomic operation to enable user middleware
+    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    const task = await Task.findById(req.params.id)
+
+    if (!task) {
+      return req.status(404).send()
     }
 
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    updates.forEach((update) => {
+      user[update] = req.body[update]
+    })
 
-        if (!task) {
-            req.status(404).send()
-        }
-        res.send(task)
-    } catch (error) {
-        res.status(500).send(error)
-    }
+    await task.save()
+    res.send(task)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
 router.delete('/tasks/:id', async (req, res) => {
-    try {
-        const id = await Task.findByIdAndDelete(req.params.id)
-        if (!id) {
-            return res.status(404).send()
-        }
-        res.send()
-    } catch (error) {
-        res.status(400).send()
+  try {
+    const id = await Task.findByIdAndDelete(req.params.id)
+    if (!id) {
+      return res.status(404).send()
     }
+    res.send()
+  } catch (error) {
+    res.status(400).send()
+  }
 })
 
 module.exports = router
