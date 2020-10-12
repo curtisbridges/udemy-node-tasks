b53329f7cfd92d5d8bca4783585598e45cd99acc
Curtis Bridges <curtis@curtisbridges.com>
2020-10-12 09:35:58 -0400

    handle image uploads for avatars
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 
@@ -85,6 +86,24 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (error) {
     res.status(400).send()
   }
+})
+
+const upload = multer({
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'))
+    }
+
+    cb(undefined, true)
+  }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send()
 })
 
 module.exports = router
