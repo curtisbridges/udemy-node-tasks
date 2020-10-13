62db1ed70283d18e9e217fc399e6ef02281ea514
Curtis Bridges <curtis@curtisbridges.com>
2020-10-13 09:45:22 -0400

    rename test directory to tests
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- tests/user.test.js
+++ tests/user.test.js
@@ -102,3 +102,11 @@ test('Should not delete account for unauthenticated user', async () => {
     .send()
     .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200)
+})
