62e72c19f86e318ac75d0f35d02ffae6198bdafc
Curtis Bridges <curtis@curtisbridges.com>
2020-10-13 10:47:04 -0400

    additional task tests
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- tests/task.test.js
+++ tests/task.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
 
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const { userOne, userTwo, taskOne, setupDatabase } = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
@@ -18,4 +18,25 @@ test('Should create a task for user', async () => {
   const task = await Task.findById(response.body._id)
   expect(task).not.toBeNull()
   expect(task.completed).toEqual(false)
+})
+
+test('Fetch user tasks', async () => {
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+
+  expect(response.body.length).toEqual(2)
+})
+
+test('Only task owner should be able to delete a task', async () => {
+  await request(app)
+    .delete(`/task/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404)
+
+  const task = Task.findById(taskOne._id)
+  expect(task).not.toBeNull()
 })
